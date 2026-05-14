0c859df49252da93a1be02ed6988f59d56399bf1
logesh-aimwindow <Logeshwaran.p@aimwindow.co.in>
2026-05-14 22:47:42 +0530

    feat: new api clients/contact

--- src/controller/routes/router.js
+++ src/controller/routes/router.js
@@ -132,6 +132,67 @@ router.post("/client/contact", async (req, res) => {
   }
 });
 
+router.post("/clients/contact", async (req, res) => {
+  console.log("Client Contact Req Body:", req.body);
+  const { name, email, message, phone, source, project, secondaryPhone } =
+    req.body;
+
+  if (!name || !phone)
+    return res.status(400).json({ message: "All fields required" });
+
+  let mailStatus = "success";
+
+  try {
+    await sendClientContactMail({
+      name,
+      email,
+      message: message || "",
+      phone,
+      project,
+      secondaryPhone,
+    });
+  } catch (error) {
+    console.error("Error sending mail:", error);
+    mailStatus = "failed";
+  }
+
+  //   if (mailStatus === "success") {
+  //     res.status(200).json({ message: "Thank you for contacting" });
+  //   } else {
+  //     res.status(500).json({ message: "Mail failed. Lead still saved." });
+  //   }
+
+  // Save lead regardless of mail success/failure
+  try {
+    const leadResp = await Lead.create({
+      name,
+      email,
+      phone,
+      message: message || "",
+      mailStatus,
+      secondaryPhone,
+      source: source || "PORTAL",
+      project,
+    });
+    if (leadResp._id) {
+      return res.status(200).json({
+        status: 200,
+        message: "Thank you for contacting us. We will get back to you soon.",
+      });
+    }
+    return res.json({
+      status: 500,
+      message: "Failed to Submit, Please try again later.",
+    });
+  } catch (dbErr) {
+    console.error("Failed to save lead:", dbErr);
+    return res.json({
+      status: 500,
+      message: "Failed to Submit, Please try again later.",
+    });
+  }
+});
+
 const VERIFY_TOKEN = "fb_leads_secret"; // use the same in FB webhook setup
 const PAGE_ACCESS_TOKEN =
   "EAALhuwyEBSsBPiwcjgY8OP639yN2tkZBmm3hx3Vi7suSnLOljNTF5AUoRXZBVhTAT0gPzMfnjpoWcZAqMP4KEBcWQWOZCgKTH4dZBZCpZCU6FygXM7MKskj715hbq7wSAvYbZCPJTNlhrTsFYJjeWxLwXooiWsW2ZCUMePMUac5gugFOybDV7tKZAQCF9nxP2mNsYRu0ePSICf73lV1DM80ssX"; // will be added later
